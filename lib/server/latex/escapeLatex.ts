d4272368c5df8cb586b1cf1e3da1c2b9f6cbd049
Eddy <17368139916@163.com>
2026-02-04 09:24:47 +1100

    fix: strip invalid unicode for LaTeX

--- lib/server/latex/escapeLatex.ts
+++ lib/server/latex/escapeLatex.ts
@@ -3,7 +3,10 @@ export function escapeLatex(value: string) {
     .toString("utf8")
     .normalize("NFKC")
     .replace(/\r\n/g, "\n")
-    .replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/g, " ");
+    .replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/g, " ")
+    .replace(/[\uD800-\uDFFF]/g, "")
+    .replace(/\uFFFD/g, "")
+    .replace(/[\u{1F000}-\u{10FFFF}]/gu, "");
 
   return normalized
     .replace(/\\/g, "\\\\")
