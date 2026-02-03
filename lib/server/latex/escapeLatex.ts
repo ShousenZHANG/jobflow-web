13f1528d0ec3619258018f0825c564a066bba9e5
Eddy <17368139916@163.com>
2026-02-04 01:51:47 +1100

    fix: sanitize resume text before LaTeX

--- lib/server/latex/escapeLatex.ts
+++ lib/server/latex/escapeLatex.ts
@@ -1,5 +1,11 @@
 export function escapeLatex(value: string) {
-  return value
+  const normalized = Buffer.from(value ?? "", "utf8")
+    .toString("utf8")
+    .normalize("NFKC")
+    .replace(/\r\n/g, "\n")
+    .replace(/[\u0000-\u0008\u000B\u000C\u000E-\u001F\u007F]/g, " ");
+
+  return normalized
     .replace(/\\/g, "\\\\")
     .replace(/&/g, "\\&")
     .replace(/%/g, "\\%")
